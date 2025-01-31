03cba49a4a2cb5b4bb73234832fa2cf4b133900b
MattiasGervilliers <mattias.gervilliers@etu.univ-amu.fr>
2025-01-31 22:11:40 +0100

    Initialize SaveEngine and EasySaveConsole projects

---
+++ SaveEngine/SaveConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace SaveEngine
+{
+    internal class SaveConfiguration
+    {
+        private string _name { get; set; }
+        private Path _sourcePath { get; set; }
+        private Path _destinationPath { get; set; }
+
+    }
+}
