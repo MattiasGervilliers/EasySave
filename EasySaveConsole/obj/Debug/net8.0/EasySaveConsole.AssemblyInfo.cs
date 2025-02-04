9bb6632cf8ad49f814a061e411f5103aef2aec26
Ninobt <nino.benhamel@viacesi.fr>
2025-02-04 14:58:59 +0100

    Add view,model,controller for easySave console

--- EasySaveConsole/obj/Debug/net8.0/EasySaveConsole.AssemblyInfo.cs
+++ EasySaveConsole/obj/Debug/net8.0/EasySaveConsole.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EasySaveConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+346e76ecbb498b7326e5dfca32b4a5c31123a00f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54d6ff86e8f5dabc097a1e768ce7bf70c5c17d21")]
 [assembly: System.Reflection.AssemblyProductAttribute("EasySaveConsole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EasySaveConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
