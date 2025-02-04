1ee55ce5a577f16eb3d190aa9eba46dbcec786e3
Ninobt <nino.benhamel@viacesi.fr>
2025-02-04 11:06:34 +0100

    Add BackupService.cs and View.cs

---
+++ EasySaveConsole/BackupService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using SaveEngine;
+
+namespace EasySaveConsole
+{
+    internal class BackupService
+    {
+        private 
+        
+        public BackupService() 
+        { 
+            
+        }
+
+    }
+}
