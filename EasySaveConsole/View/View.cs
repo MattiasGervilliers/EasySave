1db83f3897270c84de15408ef110d491e31326b2
Ninobt <nino.benhamel@viacesi.fr>
2025-02-05 14:26:48 +0100

    Edit View for english and french

--- EasySaveConsole/View/View.cs
+++ EasySaveConsole/View/View.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -55,29 +56,65 @@ namespace EasySaveConsole.View
         public void AfficheChoixLangue()
         {
             Console.WriteLine("Voici les langues disponnibles / Choose a language :");
-            Console.WriteLine("1 - " + Language.English);
-            Console.WriteLine("2 - " + Language.French);
+            Console.WriteLine("1 - Anglais/" + Language.English);
+            Console.WriteLine("2 - Français/" + Language.French);
         }
         public void AfficheNom()
         {
-            Console.WriteLine("Bienvenu dans le menu de création de configuration de sauvegarde");
-            Console.WriteLine("Veullez entrer le nom de la configuration : ");
+            if (this.Langue == Language.French)
+            {
+                Console.WriteLine("Bienvenue dans le menu de création de configuration de sauvegarde");
+                Console.WriteLine("Veuillez entrer le nom de la configuration : ");
+            }
+            else
+            {
+                Console.WriteLine("Welcome to the backup configuration creation menu");
+                Console.WriteLine("Please enter the name of the configuration: ");
+            }
         }
+
         public void AfficheFichierSource()
         {
-            Console.WriteLine("Veullez entrer le fichier source que vous souhaitez sauvegarder ");
+            if (this.Langue == Language.French)
+            {
+                Console.WriteLine("Veuillez entrer le fichier source que vous souhaitez sauvegarder : ");
+            }
+            else
+            {
+                Console.WriteLine("Please enter the source file you want to back up: ");
+            }
         }
+
         public void AfficheFichierCible()
         {
-            Console.WriteLine("Veullez entrer le fichier cible que vous souhaitez sauvegarder ");
+            if (this.Langue == Language.French)
+            {
+                Console.WriteLine("Veuillez entrer le fichier cible où vous souhaitez sauvegarder : ");
+            }
+            else
+            {
+                Console.WriteLine("Please enter the target file where you want to save: ");
+            }
         }
+
         public void AfficheType()
         {
-            Console.WriteLine("Choisissez un type de sauvegarde :");
-            Console.WriteLine("1 - Full");
-            Console.WriteLine("2 - Incremental");
-            Console.Write("Votre choix (1 ou 2) : ");
+            if (this.Langue == Language.French)
+            {
+                Console.WriteLine("Choisissez un type de sauvegarde :");
+                Console.WriteLine("1 - Complète");
+                Console.WriteLine("2 - Incrémentale");
+                Console.Write("Votre choix (1 ou 2) : ");
+            }
+            else
+            {
+                Console.WriteLine("Choose a backup type:");
+                Console.WriteLine("1 - Full");
+                Console.WriteLine("2 - Incremental");
+                Console.Write("Your choice (1 or 2): ");
+            }
         }
+
         public void AfficheQuitter()
         {
             if (Langue == Language.French)
