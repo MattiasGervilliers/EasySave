10fa39421132ac19b8caa88cb767bae934678142
Mattias Gervilliers <91268716+MattiasGervilliers@users.noreply.github.com>
2025-02-12 10:58:29 +0100

    Update ControllerCreer.cs

--- EasySaveConsole/Controller/ControllerCreer.cs
+++ EasySaveConsole/Controller/ControllerCreer.cs
@@ -50,7 +50,7 @@ namespace EasySaveConsole.Controller
                 if (input == "1")
                     return BackupType.Full;
                 else if (input == "2")
-                    return BackupType.Differential;
+                    return BackupType.Incremental;
                 else
                 {
                     if (language == Language.French)
