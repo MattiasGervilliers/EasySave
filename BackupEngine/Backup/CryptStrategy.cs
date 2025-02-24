50e2baaac3b078bbefbd1a26b3fb9830a8716566
Mattias GERVILLIERS <watgytb@gmail.com>
2025-02-24 14:33:14 +0100

    Update workflow file

--- BackupEngine/Backup/CryptStrategy.cs
+++ BackupEngine/Backup/CryptStrategy.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
+﻿using System.Diagnostics;
 using System.Net;
-using System.Runtime.InteropServices;
 using System.Security;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BackupEngine.Backup
 {
